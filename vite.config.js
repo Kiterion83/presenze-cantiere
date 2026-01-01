13b3a554d5b7918a708f1d9c875d68d463ebf0f4
Kiterion83 <giuseppe.pasquale@outlook.com>
2026-01-01 20:23:46 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    host: true
-  },
   build: {
-    outDir: 'dist',
-    sourcemap: false
+    chunkSizeWarningLimit: 1000  // Aumentato da 500KB a 1000KB
   }
 })
