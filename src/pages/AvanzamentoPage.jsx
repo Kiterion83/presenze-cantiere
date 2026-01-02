e2a5c53c904b636f2b7bee1996935ce59d4f72fe
Kiterion83 <giuseppe.pasquale@outlook.com>
2026-01-02 08:00:46 +0100

    Add files via upload

---
+++ src/pages/AvanzamentoPage.jsx
@@ -0,0 +1,6 @@
+// AvanzamentoPage.jsx - Wrapper page
+import AvanzamentoPage from '../components/AvanzamentoPage'
+
+export default function AvanzamentoPageWrapper() {
+  return <AvanzamentoPage />
+}
