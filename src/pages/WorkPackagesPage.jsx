06624cf59bb94f2ffd87549cd72f2cc379f2fb24
Kiterion83 <giuseppe.pasquale@outlook.com>
2026-01-02 01:08:55 +0100

    Add files via upload

---
+++ src/pages/WorkPackagesPage.jsx
@@ -0,0 +1,6 @@
+// WorkPackagesPage.jsx - Wrapper page per Work Packages
+import WorkPackagesPage from '../components/WorkPackagesPage'
+
+export default function WorkPackagesPageWrapper() {
+  return <WorkPackagesPage />
+}
